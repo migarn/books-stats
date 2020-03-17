d1fd0b1ebca9d14cab2e7c37a18480667ac6e9a5
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-03-17 18:14:21 +0100

    Working code

--- src/com/garnczarski/michal/App.java
+++ src/com/garnczarski/michal/App.java
@@ -2,30 +2,42 @@ package com.garnczarski.michal;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) throws FileNotFoundException {
+		showStats("books.txt");
+	}
+
+	public static void showStats(String pathToFile) {
 		Parser parser = new Parser();
 		Stats stats = new Stats();
 		List<Book> booklist;
 
-		
 		try {
-			booklist = parser.parse("books.txt");
+			booklist = parser.parse(pathToFile);
 			stats.countBooksPerCategory(booklist);
 			stats.topAuthors(booklist);
-			stats.topAuthorsInCategory(booklist, "kategoria 1");
-			stats.topAuthorsInCategory(booklist, "fantastyka");
-			
-//			for (Book book : booklist) {
-//				System.out.println(book.getAuthor() + " " + book.getTitle() + " " + book.getCategory());
-//				System.out.println();
-//			}
-		}
-		catch (Exception e) {
+			String readCategory = readCategory(booklist);
+			stats.topAuthorsInCategory(booklist, readCategory);
+		} catch (Exception e) {
 			System.out.println("File not found.");
-		}	
+		}
 	}
 
+	public static String readCategory(List<Book> booklist) {
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("\nType category for printing its top authors:");
+			String category = scanner.nextLine();
+			for (Book book : booklist) {
+				if (book.getCategory().equals(category)) {
+					return category;
+				}
+			}
+			System.out.println("\nCategory not found.");
+		}
+	}
 }
